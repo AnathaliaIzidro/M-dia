897fe47f77157ce755a8d59e3e88a37d13468ade
Anathalia Izidro <49588396+AnathaliaIzidro@users.noreply.github.com>
2024-07-12 10:42:57 -0300

    Add files via upload

---
+++ notas.py
@@ -0,0 +1,23 @@
+# 1. Crie três variáveis e armazene três notas nelas. Em seguida crie uma variável para salvar a média destas notas. Exiba a média usando o comando print
+
+# nota1 = 9.5
+# nota2 = 10
+# nota3 = 6.5
+# media = (nota1 + nota2 + nota3) / 3
+# print("Média =", media)
+# print(f'Média = {media:.2f}')
+
+nota1 = 5
+nota2 = 8 
+nota3 = 9
+
+media = (nota1 + nota2 + nota3) / 3
+
+peso1 = 2
+
+peso2 = 3
+
+mediaPonderada = ((nota1 * peso1) + (nota2 * peso2) + nota3) / 6
+
+print(f'A média é : {media:.2f}')
+print(f'A média Ponderada é : {mediaPonderada:.2f}')
